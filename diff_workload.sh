78ba2e350178dbd41f05b2a3788580a4d7de00e0
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-11-17 21:05:54 -0500

    fix scripts

--- diff_workload.sh
+++ diff_workload.sh
@@ -17,13 +17,13 @@ for workload in ${arr[@]}; do
 	fi
 
 	if ! test -f ./diff_workloads/fifo_per_cpu_agent_$workload.out.done; then
-		sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 10000 151000 10000 fifo_per_cpu_agent 30s | tee ./diff_workloads/fifo_per_cpu_agent_$workload.out
+		sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 30us 10000 151000 10000 fifo_per_cpu_agent 30s | tee ./diff_workloads/fifo_per_cpu_agent_$workload.out
 		mv ./diff_workloads/fifo_per_cpu_agent_$workload.out ./diff_workloads/fifo_per_cpu_agent_$workload.out.done
 		echo "Done fifo_per_cpu_agent_$workload.out"
 	fi
 
 	if ! test -f ./diff_workloads/agent_sol_$workload.out.done; then
-		sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 10000 151000 10000 agent_sol 30s | tee ./diff_workloads/agent_sol_$workload.out
+		sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 30us 10000 151000 10000 agent_sol 30s | tee ./diff_workloads/agent_sol_$workload.out
 		mv ./diff_workloads/agent_sol_$workload.out ./diff_workloads/agent_sol_$workload.out.done
 		echo "Done agent_sol_$workload.out"
 	fi
