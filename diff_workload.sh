05f8612449f514ba98ee63012d0a9d3d4c26c72b
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-11-17 20:32:35 -0500

    script

--- diff_workload.sh
+++ diff_workload.sh
@@ -1,4 +1,4 @@
-arr=("0.005")
+arr=("0.005" "0.05")
 
 # 0.005 0.025 0.05 0.07 0.08 0.09 0.1
 
@@ -17,15 +17,15 @@ for workload in ${arr[@]}; do
 	fi
 
 	if ! test -f ./diff_workloads/fifo_centralized_agent_$workload.out.done; then
-		#sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 10000 151000 10000 fifo_centralized_agent 30s | tee ./diff_workloads/fifo_centralized_agent_$workload.out
-		#mv ./diff_workloads/fifo_centralized_agent_$workload.out ./diff_workloads/fifo_centralized_agent_$workload.out.done
+		sudo bazel-bin/experiments/scripts/shinjuku.par ghost $workload 10000 151000 10000 fifo_centralized_agent 30s | tee ./diff_workloads/fifo_centralized_agent_$workload.out
+		mv ./diff_workloads/fifo_centralized_agent_$workload.out ./diff_workloads/fifo_centralized_agent_$workload.out.done
 		echo "Done fifo_centralized_agent_$workload.out"
 	fi
 
-	if ! test -f ./diff_workloads/c-queue_$workload.out.done; then
-		#sudo bazel-bin/experiments/scripts/centralized_queuing.par ghost $workload 10000 151000 10000 | tee ./diff_workloads/c-queue_$workload.out
-		#mv ./diff_workloads/c-queue_$workload.out ./diff_workloads/c-queue_$workload.out.done
-		echo "Done c-queue_$workload.out"
+	if ! test -f ./diff_workloads/agent_sol_$workload.out.done; then
+		sudo bazel-bin/experiments/scripts/centralized_queuing.par ghost $workload 10000 151000 10000 agent_sol 30s | tee ./diff_workloads/agent_sol_$workload.out
+		mv ./diff_workloads/agent_sol_$workload.out ./diff_workloads/agent_sol_$workload.out.done
+		echo "Done agent_sol_$workload.out"
 	fi
 
 	
