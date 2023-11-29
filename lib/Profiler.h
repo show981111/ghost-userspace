7636aba531ba91dde19cfd908f2b1f4476b6174a
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-11-29 17:14:40 -0500

    fix namespace

--- lib/Profiler.h
+++ lib/Profiler.h
@@ -5,57 +5,59 @@
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "lib/scheduler.h"
-#include "lib/base.h"
 
 #include <vector>
-class Profiler
+
+namespace ghost
 {
-public:
-    // 1. kBlocked->kRunnable->kQueued
-    // 2. kQueued->kRunnable->kOnCpu
-    enum class TaskState
+    class Profiler
     {
-        kCreated,
-        kBlocked,
-        kRunnable,
-        kQueued,
-        kOnCpu,
-        kYielding,
-        kDied,
-        unknown
+    public:
+        // 1. kBlocked->kRunnable->kQueued
+        // 2. kQueued->kRunnable->kOnCpu
+        enum class TaskState
+        {
+            kCreated,
+            kBlocked,
+            kRunnable,
+            kQueued,
+            kOnCpu,
+            kYielding,
+            kDied,
+            unknown
+        };
+        struct Metric // Record how long it stayed in that state
+        {
+            absl::Time createdAt;        // created time
+            absl::Duration blockTime;    // Blocked state
+            absl::Duration runnableTime; // runnable state
+            absl::Duration queuedTime;   // Queued state
+            absl::Duration onCpuTime;    // OnCPU state
+            absl::Duration yieldingTime;
+            absl::Time diedAt;
+            int64_t preemptCount; // if it's preempted
+
+            TaskState currentState;
+            absl::Time stateStarted;
+
+            Metric();
+            Metric(absl::Time _createdAt) : createdAt(_createdAt), currentState(TaskState::kCreated), stateStarted(_createdAt) {}
+            void updateState(const Profiler::TaskState &newState, const absl::Duration d);
+        };
+        void update(Gtid gtid, std::string_view newState);
+        void PrintResults();
+
+    private:
+        static TaskState getStateFromString(std::string_view state);
+        absl::flat_hash_map<int64_t, Metric> metrics;
+
+        struct Result
+        {
+            Gtid gtid;
+            Metric metric;
+        };
+
+        std::vector<Profiler::Result> results;
     };
-    struct Metric // Record how long it stayed in that state
-    {
-        absl::Time createdAt;        // created time
-        absl::Duration blockTime;    // Blocked state
-        absl::Duration runnableTime; // runnable state
-        absl::Duration queuedTime;   // Queued state
-        absl::Duration onCpuTime;    // OnCPU state
-        absl::Duration yieldingTime;
-        absl::Time diedAt;
-        int64_t preemptCount; // if it's preempted
-
-        TaskState currentState;
-        absl::Time stateStarted;
-
-        Metric();
-        Metric(absl::Time _createdAt) : createdAt(_createdAt), currentState(TaskState::kCreated), stateStarted(_createdAt) {}
-        void updateState(const Profiler::TaskState &newState, const absl::Duration d);
-    };
-    void update(Gtid gtid, std::string_view newState);
-    void PrintResults();
-
-private:
-    static TaskState getStateFromString(std::string_view state);
-    absl::flat_hash_map<int64_t, Metric> metrics;
-
-    struct Result
-    {
-        Gtid gtid;
-        Metric metric;
-    };
-
-    std::vector<Profiler::Result> results;
-};
-
+}
 #endif
