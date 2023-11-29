07330a907503a3aeeecd123c3a3887903542d24b
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-11-29 17:21:50 -0500

    fix namespace

--- lib/Profiler.cc
+++ lib/Profiler.cc
@@ -1,91 +1,94 @@
 #include "lib/Profiler.h"
 
-void Profiler::Metric::updateState(const Profiler::TaskState &newState)
+namespace ghost
 {
-  absl::Time currentTime = absl::Now();
-  absl::Duration d = currentTime - stateStarted;
-  switch (currentState)
+  void Profiler::Metric::updateState(const Profiler::TaskState &newState)
   {
-  case TaskState::kBlocked:
-    blockedTime += d;
-    break;
-  case TaskState::kRunnable:
-    runnableTime += d;
-    break;
-  case TaskState::kQueued:
-    queuedTime += d;
-    break;
-  case TaskState::kOnCpu:
-    onCpuTime += d;
-    break;
-  case TaskState::kYielding:
-    yieldingTime += d;
-    break;
-  default:
-    break;
+    absl::Time currentTime = absl::Now();
+    absl::Duration d = currentTime - stateStarted;
+    switch (currentState)
+    {
+    case TaskState::kBlocked:
+      blockedTime += d;
+      break;
+    case TaskState::kRunnable:
+      runnableTime += d;
+      break;
+    case TaskState::kQueued:
+      queuedTime += d;
+      break;
+    case TaskState::kOnCpu:
+      onCpuTime += d;
+      break;
+    case TaskState::kYielding:
+      yieldingTime += d;
+      break;
+    default:
+      break;
+    }
+    currentState = newState;
   }
-  currentState = newState;
-}
 
-void Profiler::update(Gtid gtid, std::string_view newState)
-{
-  TaskState state = getStateFromString(newState);
-  int64_t rawTid = gtid.id();
+  void Profiler::update(Gtid gtid, std::string_view newState)
+  {
+    TaskState state = getStateFromString(newState);
+    int64_t rawTid = gtid.id();
 
-  if (metrics.count(rawTid)) == 0)
+    if (metrics.count(rawTid)) == 0)
     metrics.insert({rawTid, Metric(absl::Now())});
-  metrics[rawTid].updateTime(newState);
+    metrics[rawTid].updateTime(newState);
 
-  if (state == TaskState::kDied)
-  {
-    results.emplace_back(Profiler::Result{rawTid, metrics[rawTid]});
-    metrics.erase(rawTid);
-  }
-}
-
-void Profiler::PrintResults()
-{
-  //   absl::Time createdAt;        // created time
-  // absl::Duration blockTime;    // Blocked state
-  // absl::Duration runnableTime; // runnable state
-  // absl::Duration queuedTime;   // Queued state
-  // absl::Duration onCpuTime;    // OnCPU state
-  // absl::Duration yieldingTime;
-  // absl::Time diedAt;
-  // int64_t preemptCount; // if it's preempted
-
-  // TaskState currentState;
-  // absl::Time stateStarted;
-  fprintf(stdout, "=================================\n");
-
-  for (auto &res : results)
-  {
-    fprintf(stdout, "Ghost Thread Id: %" PRId64 "\n", res.gtid.id());
-    //    time_t unixTime = absl::ToUnixSeconds(currentTime);
-    fprintf(stdout, "BlockTime: %" PRId64 "\nRunnableTime: %" PRId64 "\nQueuedTime: %" PRId64 "\nonCpuTime: %" PRId64 "\nyieldingTime: %" PRId64 "\n",
-            res.metric.blockTime, res.metric.runnableTime, res.metric.queuedTime, res.metric.onCpuTime, res.metric.yieldingTime);
-    fprintf(stdout, "CreatedAt: %" PRId64 ", DiedAt: %" PRId64 "\n", absl::ToUnixSeconds(res.metric.createdAt), absl::ToUnixSeconds(res.metric.diedAt));
-    fprintf(stdout, "---------------------------------\n");
+    if (state == TaskState::kDied)
+    {
+      results.emplace_back(Profiler::Result{rawTid, metrics[rawTid]});
+      metrics.erase(rawTid);
+    }
   }
 
-  fprintf(stdout, "=================================\n");
-}
-
-Profiler::TaskState Profiler::getStateFromString(std::string_view state)
-{
-  if (state == "Blocked")
-    return Profiler::TaskState::kBlocked;
-  else if (state == "Runnable")
-    return Profiler::TaskState::kRunnable;
-  else if (state == "Queued")
-    return Profiler::TaskState::kQueued;
-  else if (state == "onCpu")
-    return Profiler::TaskState::kOnCpu;
-  else if (state == "yielding")
-    return Profiler::TaskState::kYielding;
-  else
+  void Profiler::PrintResults()
   {
-    fprintf(stderr, "Task state is unknown(%s)\n", state.data());
-    return Profiler::TaskState::unknown;
+    //   absl::Time createdAt;        // created time
+    // absl::Duration blockTime;    // Blocked state
+    // absl::Duration runnableTime; // runnable state
+    // absl::Duration queuedTime;   // Queued state
+    // absl::Duration onCpuTime;    // OnCPU state
+    // absl::Duration yieldingTime;
+    // absl::Time diedAt;
+    // int64_t preemptCount; // if it's preempted
+
+    // TaskState currentState;
+    // absl::Time stateStarted;
+    fprintf(stdout, "=================================\n");
+
+    for (auto &res : results)
+    {
+      fprintf(stdout, "Ghost Thread Id: %" PRId64 "\n", res.gtid.id());
+      //    time_t unixTime = absl::ToUnixSeconds(currentTime);
+      fprintf(stdout, "BlockTime: %" PRId64 "\nRunnableTime: %" PRId64 "\nQueuedTime: %" PRId64 "\nonCpuTime: %" PRId64 "\nyieldingTime: %" PRId64 "\n",
+              res.metric.blockTime, res.metric.runnableTime, res.metric.queuedTime, res.metric.onCpuTime, res.metric.yieldingTime);
+      fprintf(stdout, "CreatedAt: %" PRId64 ", DiedAt: %" PRId64 "\n", absl::ToUnixSeconds(res.metric.createdAt), absl::ToUnixSeconds(res.metric.diedAt));
+      fprintf(stdout, "---------------------------------\n");
+    }
+
+    fprintf(stdout, "=================================\n");
+  }
+
+  Profiler::TaskState Profiler::getStateFromString(std::string_view state)
+  {
+    if (state == "Blocked")
+      return Profiler::TaskState::kBlocked;
+    else if (state == "Runnable")
+      return Profiler::TaskState::kRunnable;
+    else if (state == "Queued")
+      return Profiler::TaskState::kQueued;
+    else if (state == "onCpu")
+      return Profiler::TaskState::kOnCpu;
+    else if (state == "yielding")
+      return Profiler::TaskState::kYielding;
+    else
+    {
+      fprintf(stderr, "Task state is unknown(%s)\n", state.data());
+      return Profiler::TaskState::unknown;
+    }
   }
 }
